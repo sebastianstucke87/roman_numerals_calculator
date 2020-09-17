271fde37a182f4a4c6b806178738c4e2764a7e23
Sebastian Stucke <sebastianstucke87@gmail.com>
2020-09-17 20:50:58 +0200

    Init: project layout

---
+++ tests/Smoke/PHPUnitSmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smoke;
+
+use PHPUnit\Framework\TestCase;
+
+final class PHPUnitSmokeTest extends TestCase
+{
+    public function test_that_phpunit_setup_works()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/RomanNumerals/ConverterTest.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Unit\RomanNumerals;
+
+use PHPUnit\Framework\TestCase;
+
+final class ConverterTest extends TestCase
+{
+    // NOP
+}
