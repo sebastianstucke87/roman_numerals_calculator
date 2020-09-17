dfec331fa8dab792091c8f4549bfee53b857036d
Sebastian Stucke <sebastianstucke87@gmail.com>
2020-09-17 21:59:06 +0200

    Calculator: define API

---
+++ src/RomanNumeralsCalculator/Calculator.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace RomanNumeralsCalculator;
+
+final class Calculator
+{
+    public function sum(int ...$numbers): int
+    {
+        // NOP
+    }
+}
--- tests/Unit/RomanNumerals/CalculatorTest.php
+++ tests/Unit/RomanNumerals/CalculatorTest.php
@@ -3,9 +3,45 @@ declare(strict_types=1);
 
 namespace Unit\RomanNumerals;
 
+use LogicException;
 use PHPUnit\Framework\TestCase;
+use RomanNumeralsCalculator\Calculator;
 
 final class CalculatorTest extends TestCase
 {
-    // NOP
+    public function test_it_needs_at_least_one_number()
+    {
+        // arrange
+        $sut = new Calculator();
+
+        // assert
+        $this->expectException(LogicException::class);
+
+        // act
+        $result = $sut->sum();
+    }
+
+    public function test_it_can_add_one_number()
+    {
+        // arrange
+        $sut = new Calculator();
+
+        // act
+        $result = $sut->sum(1);
+
+        // assert
+        $this->assertEquals(1, $result);
+    }
+
+    public function test_it_can_add_multiple_numbers()
+    {
+        // arrange
+        $sut = new Calculator();
+
+        // act
+        $result = $sut->sum(1, 2, 3, -4, 40);
+
+        // assert
+        $this->assertEquals(42, $result);
+    }
 }
