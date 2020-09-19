e43a48a3e12b201bfb6b8ca26c16aaf6b6a7d953
Sebastian Stucke <sebastianstucke87@gmail.com>
2020-09-19 14:16:26 +0200

    Calculator: order TCs by value, split roman/arabic edge cases

--- tests/Unit/RomanNumerals/ConverterTest.php
+++ tests/Unit/RomanNumerals/ConverterTest.php
@@ -14,7 +14,7 @@ final class ConverterTest extends TestCase
      * @dataProvider provide_basic_tenths
      * @dataProvider provide_basic_hundreds
      * @dataProvider provide_wikipedia_examples
-     * @dataProvider provide_edge_cases
+     * @dataProvider provide_roman_edge_cases
      */
     public function test_it_can_convert_from_roman_to_arabic_numerals(int $arabic, string $roman): void
     {
@@ -34,7 +34,7 @@ final class ConverterTest extends TestCase
      * @dataProvider provide_basic_tenths
      * @dataProvider provide_basic_hundreds
      * @dataProvider provide_wikipedia_examples
-     * @dataProvider provide_edge_cases
+     * @dataProvider provide_arabic_edge_cases
      */
     public function test_it_can_convert_from_arabic_to_roman_numerals(int $arabic, string $roman): void
     {
@@ -52,13 +52,13 @@ final class ConverterTest extends TestCase
     {
         // array schema: [arabic, roman]
         return [
-            [1, 'I'],
-            [5, 'V'],
-            [10, 'X'],
-            [50, 'L'],
-            [100, 'C'],
-            [500, 'D'],
-            [1000, 'M'],
+            "1 - I" => [1, 'I'],
+            "5 - V" => [5, 'V'],
+            "10 - X" => [10, 'X'],
+            "50 - L" => [50, 'L'],
+            "100 - C" => [100, 'C'],
+            "500 - D" => [500, 'D'],
+            "1000 - M" => [1000, 'M'],
         ];
     }
 
@@ -66,16 +66,16 @@ final class ConverterTest extends TestCase
     {
         // array schema: [arabic, roman]
         return [
-            [1, 'I'],
-            [2, 'II'],
-            [3, 'III'],
-            [4, 'IV'],
-            [5, 'V'],
-            [6, 'VI'],
-            [7, 'VII'],
-            [8, 'VIII'],
-            [9, 'IX'],
-            [10, 'X'],
+            '1 - I' => [1, 'I'],
+            '2 - II' => [2, 'II'],
+            '3 - III' => [3, 'III'],
+            '4 - IV' => [4, 'IV'],
+            '5 - V' => [5, 'V'],
+            '6 - VI' => [6, 'VI'],
+            '7 - VII' => [7, 'VII'],
+            '8 - VIII' => [8, 'VIII'],
+            '9 - IX' => [9, 'IX'],
+            '10 - X' => [10, 'X'],
         ];
     }
 
@@ -83,16 +83,16 @@ final class ConverterTest extends TestCase
     {
         // array schema: [arabic, roman]
         return [
-            [11, 'XI'],
-            [20, 'XX'],
-            [30, 'XXX'],
-            [40, 'XL'],
-            [50, 'L'],
-            [60, 'LX'],
-            [70, 'LXX'],
-            [80, 'LXXX'],
-            [90, 'XC'],
-            [100, 'C'],
+            '11 - XI' => [11, 'XI'],
+            '20 - XX' => [20, 'XX'],
+            '30 - XXX' => [30, 'XXX'],
+            '40 - XL' => [40, 'XL'],
+            '50 - L' => [50, 'L'],
+            '60 - LX' => [60, 'LX'],
+            '70 - LXX' => [70, 'LXX'],
+            '80 - LXXX' => [80, 'LXXX'],
+            '90 - XC' => [90, 'XC'],
+            '100 - C' => [100, 'C'],
         ];
     }
 
@@ -100,16 +100,16 @@ final class ConverterTest extends TestCase
     {
         // array schema: [arabic, roman]
         return [
-            [200, 'CC'],
-            [300, 'CCC'],
-            [400, 'CD'],
-            [500, 'D'],
-            [600, 'DC'],
-            [700, 'DCC'],
-            [800, 'DCCC'],
-            [900, 'CM'],
-            [1000, 'M'],
-            [1001, 'MI'],
+            '200 - CC' => [200, 'CC'],
+            '300 - CCC' => [300, 'CCC'],
+            '400 - CD' => [400, 'CD'],
+            '500 - D' => [500, 'D'],
+            '600 - DC' => [600, 'DC'],
+            '700 - DCC' => [700, 'DCC'],
+            '800 - DCCC' => [800, 'DCCC'],
+            '900 - CM' => [900, 'CM'],
+            '1000 - M' => [1000, 'M'],
+            '1001 -  MI' => [1001, 'MI'],
         ];
     }
 
@@ -120,47 +120,54 @@ final class ConverterTest extends TestCase
     {
         // array schema: [arabic, roman]
         return [
-            [39, 'XXXIX'],
-            [246, 'CCXLVI'],
-            [789, 'DCCLXXXIX'],
-            [2421, 'MMCDXXI'],
-            [160, 'CLX'],
-            [207, 'CCVII'],
-            [1009, 'MIX'],
-            [1066, 'MLXVI'],
-            [1776, 'MDCCLXXVI'],
-            [1918, 'MCMXVIII'],
-            [1954, 'MCMLIV'],
-            [2014, 'MMXIV'],
-            [90, 'XC'],
-            [45, 'XLV'],
-            [99, 'XCIX'],
-            [18, 'XVIII'],
-            [19, 'XIX'],
-            [14, 'XIV'],
-            [1894, 'MDCCCXCIV'],
-            [4, 'IV'],
-            [40, 'XL'],
-            [400, 'CD'],
-            [9, 'VIIII'],
-            [90, 'LXXXX'],
-            [900, 'DCCCC'],
+            '4 - IV' => [4, 'IV'],
+            '9 - VIIII' => [9, 'VIIII'],
+            '14 - XIV' => [14, 'XIV'],
+            '18 - XVIII' => [18, 'XVIII'],
+            '19 - XIX' => [19, 'XIX'],
+            '39 - XXXIX' => [39, 'XXXIX'],
+            '40 - XL' => [40, 'XL'],
+            '45 - XLV' => [45, 'XLV'],
+            '90 - LXXXX' => [90, 'LXXXX'],
+            '90 - XC' => [90, 'XC'],
+            '99 - XCIX' => [99, 'XCIX'],
+            '160 - CLX' => [160, 'CLX'],
+            '207 - CCVII' => [207, 'CCVII'],
+            '246 - CCXLVI' => [246, 'CCXLVI'],
+            '400 - CD' => [400, 'CD'],
+            '789 - DCCLXXXIX' => [789, 'DCCLXXXIX'],
+            '900 - DCCCC' => [900, 'DCCCC'],
+            '1009 - MIX' => [1009, 'MIX'],
+            '1066 - MLXVI' => [1066, 'MLXVI'],
+            '1776 - MDCCLXXVI' => [1776, 'MDCCLXXVI'],
+            '1894 - MDCCCXCIV' => [1894, 'MDCCCXCIV'],
+            '1918 - MCMXVIII' => [1918, 'MCMXVIII'],
+            '1954 - MCMLIV' => [1954, 'MCMLIV'],
+            '2014 - MMXIV' => [2014, 'MMXIV'],
+            '2421 - MMCDXXI' => [2421, 'MMCDXXI'],
         ];
     }
 
-    public function provide_edge_cases(): array
+    public function provide_arabic_edge_cases(): array
     {
         // array schema: [arabic, roman]
         return [
-            [-1, ''],
-            [0, ''],
-            [1, 'i'],
-            [5, 'v'],
-            [10, 'x'],
-            [50, 'l'],
-            [100, 'c'],
-            [500, 'd'],
-            [1000, 'm'],
+            '-1 - ""' => [-1, ''],
+            '0 - ""' => [0, ''],
+        ];
+    }
+
+    public function provide_roman_edge_cases()
+    {
+        // array schema: [arabic, roman]
+        return [
+            '1 - i' => [1, 'i'],
+            '5 - v' => [5, 'v'],
+            '10 - x' => [10, 'x'],
+            '50 - l' => [50, 'l'],
+            '100 - c' => [100, 'c'],
+            '500 - d' => [500, 'd'],
+            '1000 - m' => [1000, 'm'],
         ];
     }
 }
